a208445a2fe789eebc509c6f6117db7a8abc301a
Sumit <sumit500241@gmail.com>
2025-12-20 19:56:11 +0530

    Build connection, message and discover page

--- client/src/pages/Discover.jsx
+++ client/src/pages/Discover.jsx
@@ -1,7 +1,68 @@
-import React from "react";
+import { useState } from "react";
+import { dummyConnectionsData } from "../assets/assets.js";
+import { Search } from "lucide-react";
+import UserCard from "../components/UserCard.jsx";
+import Loading from "../components/Loading.jsx";
 
 const Discover = () => {
-  return <div>Discover</div>;
+  const [input, setInput] = useState("");
+  const [users, setUsers] = useState(dummyConnectionsData);
+  const [loading, setLoading] = useState(false);
+
+  const handleSearch = async (e) => {
+    if (e.key === "Enter") {
+      setUsers([]);
+      setLoading(true);
+      setTimeout(() => {
+        setUsers(dummyConnectionsData);
+        setLoading(false);
+      }, 1000);
+    }
+  };
+
+  return (
+    <div className="min-h-screen relative bg-linear-to-br from-violet-100 to-sky-100 pl-0 lg:pl-60 xl:pl-72 no-scrollbar overflow-hidden">
+      <div className="absolute z-0 -top-30 -right-30 w-150 h-150 bg-linear-to-bl from-indigo-300/50 to-sky-300/30 rounded-full blur-2xl" />
+      <div className="absolute z-0 -bottom-10 -left-2 w-200 h-120 bg-linear-to-tr from-purple-300/20 bg-indigo-500/20 rounded-full blur-3xl" />
+
+      <div className="relative z-10 max-w-6xl mx-auto p-6">
+        {/* Title */}
+        <div className="mb-8">
+          <h1 className="text-3xl font-bold text-slate-900 mb-2">
+            Discover People
+          </h1>
+          <p className="text-slate-600">
+            Connect with amazing people and grow your network
+          </p>
+        </div>
+
+        {/* Search */}
+        <div className="mb-8 shadow-md rounded-md border border-slate-200/60 bg-white/80">
+          <div className="p-6">
+            <div className="relative">
+              <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-slate-400 w-5 h-5" />
+              <input
+                type="text"
+                placeholder="Search people by name, username, bio, or location..."
+                className="pl-10 sm:pl-12 py-2 w-full border border-gray-300 rounded-md max-sm:text-sm"
+                onChange={(e) => setInput(e.target.value)}
+                value={input}
+                onKeyUp={handleSearch}
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-wrap gap-6">
+          {users.map((user) => (
+            <UserCard user={user} key={user._id} />
+          ))}
+        </div>
+
+        {loading && <Loading height="60vh" />}
+      </div>
+    </div>
+  );
 };
 
 export default Discover;
--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -17,12 +17,12 @@ const Layout = () => {
       </div>
       {sidebarOpen ? (
         <X
-          className="absolute top-3 right-3 p-2 z-100 bg-white rounded-md shadow w-10 h-10 text-gray-600 sm:hidden"
+          className="absolute top-3 left-3 p-2 z-100 bg-white rounded-md shadow w-10 h-10 text-gray-600 sm:hidden"
           onClick={() => setSidebarOpen(false)}
         />
       ) : (
         <Menu
-          className="absolute top-3 right-3 p-2 z-100 bg-white rounded-md shadow w-10 h-10 text-gray-600 sm:hidden"
+          className="absolute top-3 left-3 p-2 z-100 bg-white rounded-md shadow w-10 h-10 text-gray-600 sm:hidden"
           onClick={() => setSidebarOpen(true)}
         />
       )}
